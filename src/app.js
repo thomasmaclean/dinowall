ae9930e66ae0cb58eca779dd57dba20a2df62b64
Charlie Kelman <cikelman94@gmail.com>
2019-12-09 16:27:59 -0500

    Adding basic shapes and animation loop

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 /**
  * Copyright 2016 Google Inc. All Rights Reserved.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -14,16 +14,51 @@
  * limitations under the license.
  */
 
-
 var anypixel = require('anypixel');
 var ctx = anypixel.canvas.getContext2D();
 
+const INC_W = anypixel.config.width;
+const INC_H = anypixel.config.height;
+
 var colors = ['#F00', '#0F0', '#00F'];
+const color = colors[2];
+
+const dinoSize = 4;
+const horizonHeight = 2;
+
+const dinosaur = {
+  x: 0,
+  y: INC_H - dinoSize - horizonHeight,
+};
 
 /**
  * Listen for onButtonDown events and draw a 2x2 rectangle at the event site
  */
-document.addEventListener('onButtonDown', function(event) {
-	ctx.fillStyle = colors[Math.floor(Math.random() * 3)];
-	ctx.fillRect(event.detail.x - 1, event.detail.y - 1, 2, 2);
+document.addEventListener('onButtonDown', function (event) {
+  dinosaur.x++;
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+  requestAnimationFrame(update);
+});
+
+function drawHorizon() {
+  ctx.fillStyle = colors[2];
+  ctx.fillRect(0, INC_H - horizonHeight, INC_W, horizonHeight);
+}
+
+function drawDinosaur(dinosaur) {
+  ctx.fillStyle = colors[1];
+  ctx.fillRect(dinosaur.x, dinosaur.y, 4, 4);
+}
+
+function update(elapsedTime) {
+	ctx.fillStyle = '#000';
+	ctx.fillRect(0, 0, INC_W, INC_H);
+
+	drawHorizon();
+  drawDinosaur(dinosaur);
+
+  requestAnimationFrame(update);
+}
+
