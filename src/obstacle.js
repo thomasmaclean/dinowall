761c1019fc3c0eebaa68b3e1f9eaa6988bc0d0fa
Charlie Kelman <cikelman94@gmail.com>
2019-12-10 11:18:20 -0500

    adding obstacle classes

---
+++ src/obstacle.js
@@ -0,0 +1,7 @@
+export class Obstacle {
+  constructor(x, y, size = 1) {
+    this.x = x;
+    this.y = y;
+    this.size = size;
+  }
+}
---
+++ src/obstacle_manager.js
@@ -0,0 +1,9 @@
+
+
+export class ObstacleManager{
+  growingObstacles = [];
+  fallingObstacles = [];
+
+  constructor(){
+  }
+}
